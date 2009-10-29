687a18a2f9a1d2e56004be6c0c99561e8d6799bc
Christian Pointner <equinox@realraum.at>
2009-10-29 13:59:27 +0000

    updated license

--- door_daemon/sig_handler.h
+++ door_daemon/sig_handler.h
@@ -20,8 +20,9 @@
  *  This file is part of uAnytun.
  *
  *  uAnytun is free software: you can redistribute it and/or modify
- *  it under the terms of the GNU General Public License version 3 as
- *  published by the Free Software Foundation.
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  any later version.
  *
  *  uAnytun is distributed in the hope that it will be useful,
  *  but WITHOUT ANY WARRANTY; without even the implied warranty of
