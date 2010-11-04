0a7e664eb60e189f4f5227f10a16a222b1acafea
realraum <realraum@realraum.at>
2010-11-04 14:24:32 +0000

    add/remove card ids without restarting checkcard.pl

--- checkcard.pl
+++ checkcard.pl
@@ -5,20 +5,29 @@ use strict;
 my $fh;
 
 my $socketfile = $ARGV[0] || "/var/run/tuer/door_cmd.socket";
+my $keysfile = '/flash/keys';
+
 sleep(1) while (! -S $socketfile);
 my $socketaddr = sockaddr_un($socketfile);
 
-my $keys;
 my %good;
+my $keys_last_read=0;
 
-open $keys,'/flash/keys';
-while (<$keys>)
+sub read_keys
 {
-	chomp;
-	if ($_ =~ /^([0-9A-Fa-f]{8})\s+(.+)$/)
-	{
-		$good{$1}=$2;
- 	}
+  %good=();
+  my $keys;
+  open $keys,$keysfile;
+  while (<$keys>)
+  {
+    chomp;
+    if ($_ =~ /^([0-9A-Fa-f]{8})\s+(.+)$/)
+    {
+      $good{$1}=$2;
+    }
+  }
+  close $keys;
+  $keys_last_read = -M ($keysfile);
 }
 
 sub send_to_fifo
@@ -29,9 +38,15 @@ sub send_to_fifo
 	close($conn);
 }
 
+read_keys();
+
+
 while (sleep 1)
 {
 	open $fh,'/flash/tuer/mifare-read 0 2>&1 |';
+
+  read_keys() unless ($keys_last_read == -M ($keysfile));
+
 	while (<$fh>)
 	{
 		next unless /UID/;
